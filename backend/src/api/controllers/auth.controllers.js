8a87a1077013e4df49ffd3cc330c7d3992bd2358
Jason Gayle <jasonblue1626@gmail.com>
2022-01-31 04:43:45 -0500

    Fully functioning registration (without email)

--- backend/src/api/controllers/auth.controllers.js
+++ backend/src/api/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 // Node module import
 import http from "http";
+import { compare } from "bcrypt";
 
 // Model imports
 import { UserModel } from "../../db/models/user.models";
@@ -14,40 +15,119 @@ const AuthController = {
     // Validation checks
 
     // Check if the user already exists
-    const userExists = await UserModel.findOne({ email: email });
-    if (userExists) {
-      return response.status(400).json({
-        message: "An account with this email address already exists",
-      });
-    }
-
-    const userDetails = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: password,
-      type: type,
-      ip_address: request.ip,
-    };
-
-    let newUser = UserModel;
-
-    if (type == "patient") {
-      newUser = PatientModel.create(userDetails);
-    } else if (type === "doctor") {
-      newUser = DoctorModel.create(userDetails);
-    }
-
-    newUser.save((err) => {
-      if (err) {
-        return response
-          .status(400)
-          .json({ err: `Error saving ${type} ${err.message}` });
-      } else {
-        return response
-          .status(201)
-          .json({ success: `${type} Successfully saved` });
+    try {
+      const userExists = await UserModel.findOne({ email: email });
+      if (userExists) {
+        return response.status(400).json({
+          message: "An account with this email address already exists",
+        });
       }
-    });
+
+      const userDetails = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+        type: type,
+        ip_address: request.ip,
+      };
+
+      if (type == "patient") {
+        const newPatient = new PatientModel(userDetails);
+
+        newPatient.save((err) => {
+          if (err) {
+            return response
+              .status(400)
+              .json({ err: `Error saving ${type} ${err.message}` });
+          } else {
+            return response
+              .status(201)
+              .json({ success: `${type} Successfully saved` });
+          }
+        });
+      } else if (type === "doctor") {
+        const newDoctor = DoctorModel.create(userDetails);
+      } else {
+        return response.status(400).json({ err: "No User type specified" });
+      }
+    } catch (err) {
+      return response.status(500).json({ error: err.message });
+    }
+  },
+
+  async loginUser(request, response) {
+    // Extract credentials from request
+    const { email, password } = request.body;
+
+    // Perform Validation checks
+
+    // Check if the user exists
+    try {
+      const user = await UserModel.findOne({ email: email });
+      if (!user) {
+        return response.status(400).json({
+          message: "No user exists with the email entered",
+        });
+      }
+
+      // Check passwords with bcrypt
+      const passwordMatch = await compare(password, user.password);
+      if (!passwordMatch) {
+        return response.status(401).json({
+          message: "Invalid Password",
+        });
+      }
+
+      // Create the JWT
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: 900,
+      });
+
+      return response.status(200).json({
+        authToken: token,
+        user: {
+          id: user._id,
+          email: user.email,
+        },
+      });
+    } catch (err) {
+      return response.status(500).json({ error: err.message });
+    }
+  },
+
+  async checkTokenValidity(request, response) {
+    try {
+      // Check for the token
+      const token = request.header("x-access-token");
+      if (!token) {
+        return response.json({
+          message: "Token is missing",
+          isValidToken: false,
+        });
+      }
+
+      // Check if the token is still valid
+      const verifiedUser = jwt.verify(token, process.env.SECRET_KEY);
+      if (!verifiedUser) {
+        return response.json({
+          message: "Token is invalid",
+          isValidToken: false,
+        });
+      }
+
+      // Check if the token belongs to the user
+      const user = await UserModel.findById(verifiedUser.id);
+
+      if (!user) {
+        return response.json({ isValidToken: false });
+      }
+
+      return response.status(200).json({ isValidToken: true });
+    } catch (err) {
+      return response.status(500).json({ error: err.message });
+    }
   },
 };
+
+export { AuthController };
--- backend/src/api/routes/auth.routes.js
+++ backend/src/api/routes/auth.routes.js
@@ -1,12 +1,20 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 
+import { AuthController } from "../controllers/auth.controllers";
+
 const authRouter = express.Router();
 
-authRouter.post("/login", async (request, response, next) => {});
+authRouter.post("/login", async (request, response, next) => {
+  AuthController.loginUser(request, response);
+});
 
-authRouter.post("/register", async (request, response, next) => {});
+authRouter.post("/register", async (request, response, next) => {
+  AuthController.registerUser(request, response);
+});
 
-authRouter.post("/checkToken", async (request, response, next) => {});
+authRouter.post("/checkToken", async (request, response, next) => {
+  AuthController.checkTokenValidity(request, response);
+});
 
 export { authRouter };
