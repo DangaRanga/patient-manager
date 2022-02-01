220b7e33b7e11e85a7b8c1d1f705667cf115804f
Jason Gayle <jasonblue1626@gmail.com>
2022-02-01 02:07:56 -0500

    Completed basic routing and added page files

---
+++ frontend/src/components/routes/RouteClient.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Page imports
+import {
+  Login,
+  Signup,
+  Dashboard,
+  DoctorDetails,
+  NotFound,
+  UserProfile,
+} from "pages";
+
+function RouteClient() {
+  return (
+    <div>
+      This works
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/doctor/:doctorid" element={<DoctorDetails />}></Route>
+        <Route path="/account/:accountid" element={<UserProfile />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default RouteClient;
