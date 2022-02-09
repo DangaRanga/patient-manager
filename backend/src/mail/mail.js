c83aa4ba667ca4cbca9ea305090e897d3ac84c30
Jason Gayle <jasonblue1626@gmail.com>
2022-02-09 13:27:52 -0500

    Modifications to appointment model and controller, basic setup for mails

---
+++ backend/src/mail/mail.js
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+
+// Initialize Nodemailer Transporter
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+// Verify transporter
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+export { transporter };
